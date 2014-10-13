8c4bd4ebf371d86eeda18893f5aff845e6ccbb38
Pavel Simo <pavelsimo@navinfo.com>
2014-10-13 16:50:28 +0800

    Fix: Replace tabs by 4 spaces.

--- src/Drawing.cpp
+++ src/Drawing.cpp
@@ -3,35 +3,35 @@
 
 namespace draw
 {
-	void DrawLine( const math::Vector2 origin, const math::Vector2 end, 
-		LINE_TYPE type, float width )
-	{
-		glPushMatrix();
+    void DrawLine( const math::Vector2 origin, const math::Vector2 end, 
+        LINE_TYPE type, float width )
+    {
+        glPushMatrix();
 
-		// LINES
-		glLineWidth( width );
+        // LINES
+        glLineWidth( width );
 
-		if( type != SOLID )
-		{
-			glEnable ( GL_LINE_STIPPLE );
+        if( type != SOLID )
+        {
+            glEnable ( GL_LINE_STIPPLE );
 
-			if( type == DASHED )
-				glLineStipple (1, 0x00FF);   /*  dashed   */
-			else
-				glLineStipple (1, 0x0101); /* dotted */
-		}
+            if( type == DASHED )
+                glLineStipple (1, 0x00FF);   /*  dashed   */
+            else
+                glLineStipple (1, 0x0101); /* dotted */
+        }
 
-		glBegin( GL_LINES );
-			glVertex2f( origin.x, origin.y );
-			glVertex2f( end.x, end.y );
-		glEnd();
+        glBegin( GL_LINES );
+            glVertex2f( origin.x, origin.y );
+            glVertex2f( end.x, end.y );
+        glEnd();
 
-		if( type != SOLID )
-		{
-			glDisable( GL_LINE_STIPPLE );
-		}
+        if( type != SOLID )
+        {
+            glDisable( GL_LINE_STIPPLE );
+        }
 
-		glPopMatrix();	
-	}
+        glPopMatrix();  
+    }
 }
 
--- src/Drawing.h
+++ src/Drawing.h
@@ -6,10 +6,10 @@
 
 namespace draw
 {
-	enum LINE_TYPE { DOTTED, DASHED, SOLID };
+    enum LINE_TYPE { DOTTED, DASHED, SOLID };
 
-	void DrawLine( const math::Vector2 origin, const math::Vector2 end, 
-		LINE_TYPE type, float width );
+    void DrawLine( const math::Vector2 origin, const math::Vector2 end, 
+        LINE_TYPE type, float width );
 }
 
 #endif
